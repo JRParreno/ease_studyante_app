58b5390b6aa3094444573ca6314e5badee3f7dd1
JRParreno <jhonrhayparreno22@gmail.com>
2024-03-20 19:33:52 +0800

    added student list

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:ease_studyante_app/src/landing/presentation/landing_page.dart';
 import 'package:ease_studyante_app/src/teacher/pages/qr_code/presentation/teacher_qr_code_scanner.dart';
+import 'package:ease_studyante_app/src/teacher/pages/student/presentation/student_list.dart';
 import 'package:ease_studyante_app/src/teacher/teacher_home.dart';
 import 'package:flutter/material.dart';
 
@@ -24,6 +25,11 @@ Route<dynamic>? generateRoute(RouteSettings settings) {
           return const TeacherHomePage();
         case TeacherQRCodeScanner.routeName:
           return const TeacherQRCodeScanner();
+        case StudentListPage.routeName:
+          final args = settings.arguments! as StudentListArgs;
+          return StudentListPage(
+            args: args,
+          );
       }
 
       return const Scaffold(
---
+++ lib/src/teacher/pages/student/presentation/bloc/student_list_event.dart
@@ -0,0 +1,23 @@
+part of 'student_list_bloc.dart';
+
+abstract class StudentListEvent extends Equatable {
+  const StudentListEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class OnGetTeacherStudentList extends StudentListEvent {
+  final String section;
+
+  const OnGetTeacherStudentList({
+    required this.section,
+  });
+
+  @override
+  List<Object> get props => [
+        section,
+      ];
+}
+
+class OnPaginateTeacherStudentList extends StudentListEvent {}
